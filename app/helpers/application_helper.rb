91c2737a0017b5be22c53599457383e22d31918a
snebel <sam@samnebel.com>
2013-11-27 00:30:32 -0500

    sort tweets by time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,18 +4,16 @@ module ApplicationHelper
 	request = Net::HTTP::Get.new address.request_uri
 	http          = Net::HTTP.new address.host, address.port
 	http.use_ssl  = true
-	#request.oauth! http, @consumer_key, @access_token
 	request.oauth! http, OAuth::Consumer.new("3Ft58U1DWj5Rzy31mZ7hQ",
 		"tz6xjaYvhrRNZlPV2xBH7KJfqqf4v7tEef9oexj7I"),
 		OAuth::Token.new("103690150-9TzmzmL8XQcPp8FBbvZeTybWXovCcgwQnmwenMwv",
     	"R8lnWa3a9Eq563ZKUGfH2GoD965FgJUuA2Z2R6I")  
 	http.start
 	resp = http.request request
-
   end
 
   def get_tweet_html(id)
-  	address = URI("https://api.twitter.com/1/statuses/oembed.json?id=#{id}&align=center&maxwidth=500")
+  	address = URI("https://api.twitter.com/1/statuses/oembed.json?id=#{id}&omit_script=true&align=center&maxwidth=500")
 	request = Net::HTTP::Get.new address.request_uri
 	http = Net::HTTP.new address.host, address.port
 	http.use_ssl = true
@@ -24,36 +22,37 @@ module ApplicationHelper
 	JSON.parse(resp.body)["html"]
   end
 
-  def get_tweet_ids(person)
+  def get_tweets(player)
   	baseurl = "https://api.twitter.com/1.1/statuses/user_timeline.json?"
-	path = "screen_name=#{person}&count=3&include_rts=1&exclude_replies=true"
+	path = "screen_name=#{player}&count=5&include_rts=false&exclude_replies=true"
 
 	response = connect(baseurl, path)
 	tweets = JSON.parse(response.body)
-
-	ids = []
+	
+	recent_tweets = []
 	tweets.each do |tweet|
-	  ids << tweet["id"]	
+	  if DateTime.parse(tweet["created_at"]) + 3 > DateTime.now
+	    recent_tweets << tweet
+	  end
 	end
-	return ids
+	#return recent_tweets
   end
-
 
   def display_tweets(players)
-  	tweet_ids = []
+  	tweets = []
  	players.each do |player|
-	  tweet_ids << get_tweet_ids(player)
+	  tweets << get_tweets(player)
 	end
-	return show_tweets(tweet_ids)
+	tweets.flatten!
+	tweets.sort_by! { |t| DateTime.parse(t["created_at"]) }
+	show_tweets(tweets.reverse)
   end
 
-  def show_tweets(team_tweets)
-  	htmls = []
-  	team_tweets.each do |player|
-  	  player.each do |id|
-  	  	htmls << get_tweet_html(id)
-  	  end
-  	end
+  def show_tweets(tweets)
+	htmls = []
+	tweets.each do |tweet|
+	  htmls << get_tweet_html(tweet["id"])
+	end
   	return htmls
   end
 
